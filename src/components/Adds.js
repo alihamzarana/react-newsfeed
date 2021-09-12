a1e134daf3bf7cb3f07d0f3a3b59de78ff5d9f60
alihamzarana <alirajpoot241@gmail.com>
2021-09-12 22:56:17 +0500

    completed

--- src/components/Adds.js
+++ src/components/Adds.js
@@ -1,7 +1,8 @@
-import {Container, Fab, makeStyles, Modal, Tooltip } from '@material-ui/core'
+import {Button, Container, Fab, FormControlLabel, FormLabel, makeStyles, MenuItem, Modal, Radio, RadioGroup, Snackbar, TextField, Tooltip } from '@material-ui/core'
 import React , {useState} from 'react'
 import AddIcon from '@material-ui/icons/Add';
 import Add from '@material-ui/icons/Add';
+import { Alert } from '@material-ui/lab';
 
 
 const useStyles = makeStyles((theme) =>  ({
@@ -16,8 +17,21 @@ const useStyles = makeStyles((theme) =>  ({
         bottom: 0,
         left: 0,
         right: 0,
-        margin: "auto"
+        margin: "auto",
+        [theme.breakpoints.down("sm")]:{
+            width: "100vw",
+            height: "100vh"
 
+        }
+
+    },
+
+    form:{
+       padding: theme.spacing(2)
+    },
+
+    item:{
+        marginBottom: theme.spacing(3)
     },
 
     fab:{
@@ -34,6 +48,16 @@ const Adds = () => {
 
     const [open, setOpen] = useState(false)
 
+    const [openAlert, setOpenAlert] = useState(false)
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+          return;
+        }
+    
+        setOpenAlert(false);
+      };
+
 
     return (
         < >
@@ -43,8 +67,76 @@ const Adds = () => {
          </Fab>
        </Tooltip>
        <Modal open={open}>
-           <Container className={classes.container}>modal</Container>
+           <Container className={classes.container}>
+           <form autoComplete="off">
+           <div className={classes.item}>
+             <TextField  label="Title" style={{width: "100%"}}/>
+           </div>
+           <div className={classes.item}>
+             <TextField 
+              label="Description" 
+              style={{width: "100%"}}
+              id="outlined-multiline-static"
+              multiline
+              rows={4}
+              defaultValue="Tell your story..."
+              variant="outlined"/>
+           </div>
+            <div className={classes.item}>
+            <TextField select label="Visibility" value="Public">
+                <MenuItem value="Public"> Public</MenuItem>
+                <MenuItem value="Private"> Private</MenuItem>
+                <MenuItem value="Unlisted"> Unlisted</MenuItem>    
+            </TextField>
+            </div>
+            <div className={classes.item}>
+            <FormLabel component="legend">Who can comment?</FormLabel>
+               <RadioGroup >
+               <FormControlLabel 
+               value="everyone" 
+               control={<Radio />} 
+               label="Everyone" 
+               size="small"
+               />
+               
+               <FormControlLabel 
+               value="myFriends" 
+               control={<Radio />} 
+               label="My Friends" />
+
+
+              <FormControlLabel 
+              value="noone" 
+              control={<Radio />} 
+              label="No one"
+              size="small" />
+
+             <FormControlLabel 
+             value="custom" 
+             disabled control={<Radio />} 
+             label="Custom (Premium)"
+             size="small" />
+            </RadioGroup>
+
+            </div>
+            <div className={classes.item}>
+                <Button variant="outlined" color="primary" style={{marginRight:"20px"}} onClick={() => setOpenAlert(true)}>Create</Button>
+                <Button variant="outlined" color="secondary" onClick={() => setOpen(false)}>Cancel</Button>
+
+            </div>
+           </form>
+           </Container>
        </Modal>
+       <Snackbar
+        open={openAlert} 
+        autoHideDuration={4000} 
+        onClose={handleClose}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'left'}}
+        >
+        <Alert onClose={handleClose} severity="success">
+          This is a success message!
+        </Alert>
+      </Snackbar>
         </>
     )
 }
--- src/components/Rightbar.js
+++ src/components/Rightbar.js
@@ -1,12 +1,38 @@
-import { Container, makeStyles } from '@material-ui/core'
+import { Avatar, Container, Divider, ImageList, ImageListItem, Link, makeStyles, Typography } from '@material-ui/core'
+import { AvatarGroup } from '@material-ui/lab'
 import React from 'react'
 
 
 const useStyles = makeStyles((theme) =>  ({
 
      container:{
-         paddingTop: theme.spacing(10)
+         paddingTop: theme.spacing(10),
+         position:"sticky",
+         top:"0",
+
         
+     },
+     title:{
+         fontSize: "16px",
+         fontWeight: 500,
+         color: "#555"
+     },
+     imageList:{
+        width: 250,
+        height: 350,
+        
+        
+     },
+
+     category:{
+         marginTop:"20px"
+     },
+
+     link:{
+         fontSize:"16px",
+         color:"#555",
+         marginRight:theme.spacing(2)
+
      }
        
 }))
@@ -14,12 +40,60 @@ const useStyles = makeStyles((theme) =>  ({
 const Rightbar = () => {
     const classes = useStyles()
 
+    const itemData= [1,2,3,,4,5,6,7, 8]
+
 
     return (
         <Container className={classes.container}>
+            <Typography className={classes.title} gutterBottom> Online Friends</Typography>
+            <AvatarGroup max={6} style={{marginBottom: "20px"}}>
+                <Avatar alt="Trevor Henderson" src="https://material-ui.com/static/images/avatar/1.jpg" />
+                <Avatar alt="Trevor Henderson" src="https://material-ui.com/static/images/avatar/2.jpg" />
+                <Avatar alt="Remy Sharp" src="https://material-ui.com/static/images/avatar/3.jpg" />
+                <Avatar alt="Travis Howard" src="https://material-ui.com/static/images/avatar/4.jpg" />
+                <Avatar alt="Cindy Baker" src="https://material-ui.com/static/images/avatar/5.jpg" />
+                <Avatar alt="Agnes Walker" src="https://material-ui.com/static/images/avatar/6.jpg" />
+                <Avatar alt="Trevor Henderson" src="https://material-ui.com/static/images/avatar/7.jpg" />
+            </AvatarGroup>
 
-           
 
+            <Typography gutterBottom className={classes.title}>Gallery</Typography>   
+            <ImageList rowHeight={80} className={classes.imageList} cols={2}>
+            {itemData.map((item) => (
+            <ImageListItem key={item.img} cols={item.cols || 1}>
+                console.log(items.col)
+                <img src="https://source.unsplash.com/random" alt="Gallery" />
+            </ImageListItem>
+            ))}
+            </ImageList>
+
+            <Typography className={classes.category} gutterBottom> Categories</Typography>
+            <Link href="#" className={classes.link}  variant="body2">
+                Sports
+            </Link>
+            <Link href="#" className={classes.link}  variant="body2">
+                Movies
+            </Link>
+            <Link href="#" className={classes.link}  variant="body2">
+                Food
+            </Link>
+            <Link href="#" className={classes.link}  variant="body2">
+                Music
+            </Link>
+            
+            <Divider flexItem style={{marginBottom: 5}}/>
+            <Link href="#" className={classes.link}  variant="body2">
+                Science
+            </Link>
+            <Link href="#" className={classes.link}  variant="body2">
+                Islam
+            </Link>
+            <Link href="#" className={classes.link}  variant="body2">
+                Nature
+            </Link>
+            
+
+       
         </Container>
     )
 }
